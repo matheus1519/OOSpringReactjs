c7daa3ed05844ea0b1a497f16dbaa33d106ab7ca
matheus1519 <1519academico@gmail.com>
2019-11-11 14:28:55 -0300

    update, post

--- src/pages/Convidado/index.js
+++ src/pages/Convidado/index.js
@@ -1,11 +1,16 @@
 import React, {useState, useEffect} from 'react';
-import api from '../../services/api'
+import api from '../../services/api';
 
 export default function Convidado()
 {
     const [convidados, setConvidados] = useState([]);
     const [festas, setFestas] = useState([]);
-    const [convidado, setConvidado] = useState({});
+
+    const [id, setId] = useState(0);
+    const [nome, setNome] = useState('');
+    const [qtde, setQtde] = useState(0);
+    const [festaId, setFestaId] = useState(1);
+
 
 
     useEffect(() => {
@@ -14,7 +19,7 @@ export default function Convidado()
             setConvidados(response.data);
             const response2 = await api.get('/api/festas');
             setFestas(response2.data);
-            console.log(convidados);
+           console.log(response.data);
         }
         listarConvidados();
 
@@ -30,19 +35,40 @@ export default function Convidado()
 
     async function handleSubmit(e) {
         e.preventDefault();
-        console.log(convidado);
-        const response = await api.post('/api/convidados/',{
-            id: convidado.id,
-            nome : convidado.nome,
-            qtde: convidado.qtde,
-            festa: convidado.festa
-        });
+        var response;
+        if (id == 0){
+            response = await api.post('/api/convidados/',{
+                nome,
+                qtde,
+                festa:{
+                    id:festaId
+                }
+            });
+        }
+        else {
+            response = await api.put(`/api/convidados/${id}`,{
+                id,
+                nome,
+                qtde,
+                festa:{
+                    id:festaId
+                }
+            });
+        }
         if (response.status == 200)
         {
             window.location.reload();
         }
     }
 
+    async function handleUpdate(id) {
+        const response = await api.get(`/api/convidados/${id}`);
+        const convidado = response.data;
+        setId(convidado.id);
+        setNome(convidado.nome);
+        setQtde(convidado.qtde);
+        setFestaId(convidado.festa.id);
+    }
 
     return (
         <div className="container mt-2">
@@ -53,16 +79,16 @@ export default function Convidado()
                 <div className="card-body">
                     <form method="POST"  onSubmit={handleSubmit}>
                         <div className="form-group">
-                            <input type="hidden" value={convidado.id}/>
+                            <input type="hidden" value={id}/>
                             <div className="form-group">
                                 <label htmlFor="nome">Nome:</label>
-                                <input type="text" id="nome" className="form-control" onChange={(event)=>{setConvidado({nome:event.target.value})}} placeholder="Nome" value={convidado.nome}/>
+                                <input type="text" id="nome" className="form-control" onChange={(event)=>{setNome(event.target.value)}} placeholder="Nome" value={nome}/>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="acompanhantes">Acompanhantes:</label>
-                                <input type="number" id="acompanhantes" className="form-control" onChange={(event)=>{setConvidado({qtde:event.target.value})}} placeholder="Acompanhantes" value={convidado.qtde}/>
+                                <input type="number" id="acompanhantes" className="form-control" onChange={(event)=>{setQtde(event.target.value)}} placeholder="Acompanhantes" value={qtde}/>
                             </div>
-                            <select className="form-control" id="festa" value={convidado.festa}onChange={(event)=>{setConvidado({festa:event.target.value})}}>
+                            <select className="form-control" id="festa" value={festaId} onChange={(event)=>{setFestaId(event.target.value)}}>
                                 { festas.map((festa) => (
                                     <option key={festa.id} value={festa.id}>{festa.nome}</option>
                                 ))}
@@ -86,7 +112,7 @@ export default function Convidado()
                                         <td>{convidado.nome}</td>
                                         <td>{convidado.qtde}</td>
                                         <td>{convidado.festa.nome}</td>
-                                        <td><a className="btn btn-secondary glyphicon glyphicon-pencil" href={`/convidados/alterar/${convidado.id}`}>Alterar</a></td>
+                                        <td><a className="btn btn-secondary glyphicon glyphicon-pencil"onClick={()=>{handleUpdate(convidado.id)}}>Alterar</a></td>
                                         <td><a className="btn btn-outline-secondary" onClick={() => handleDelete(convidado.id)} >Excluir</a></td>
                                     </tr>
                                 )
